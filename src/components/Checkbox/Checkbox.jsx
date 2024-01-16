4fb72bb51a583a84839691a8e7d5df0e2e900de6
John Doe <fontanadeoro1983@gmail.com>
2024-01-16 18:45:24 +0100

    create Checkbox branch

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,39 @@
+import { string, array, bool, number, func } from 'prop-types'
+import { useId } from 'react'
+
+import { Controller } from 'react-hook-form'
+
+const Checkbox = ({ name, id, label, options, disabled, required, onChange }) => {
+  const inputId = is || useId()
+
+  return (
+    <Controller
+      {...{ name }}
+      render={({ field }) => {
+        return (
+          <div>
+            {label && <label>{label}</label>}
+            {options?.length ? options.map((option) => <input type="checkbox" />) : null}
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+InputSelect.propTypes = {
+  name: string.isRequired,
+  options: array.isRequired,
+  label: string,
+  disabled: bool,
+  required: bool,
+  onChange: func
+}
+
+InputSelect.defaultProps = {
+  name: 'chekboxExample',
+  disabled: false,
+  required: false
+}
+
+export default Checkbox
