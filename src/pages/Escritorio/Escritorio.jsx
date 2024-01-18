4409bef684cb2d442d03638b22863174e273ec34
John Doe <fontanadeoro1983@gmail.com>
2024-01-18 18:42:07 +0100

    create CRUD_services brach

--- src/pages/Escritorio/Escritorio.jsx
+++ src/pages/Escritorio/Escritorio.jsx
@@ -8,21 +8,25 @@ import { useNavigate } from 'react-router-dom'
 
 const Escritorio = () => {
   const GridContainer = lazy(() => import('./partials/GridContainer'))
+
   const [escritorio, setEscritorio] = useState({
     gruposDeConsumo: true,
     consumidores: false,
     hojasDeReparto: false,
     creargrupo: false
   })
+
   const navigate = useNavigate()
+
   const [consumers, setConsumers] = useState([])
   const [consumerGroups, setConsumerGroups] = useState([])
   const [consumerGroup, setConsumerGroup] = useState(null)
+
   const token = sessionStorage.getItem('token')
 
   useEffect(() => {
     if (token) {
-      get('consumer', token)
+      get('consumer')
         .then((res) => setConsumers(res))
         .catch((error) => console.log(error))
 
