356fff99a83f7a478ff8ec7c706bf6d315321df7
John Doe <fontanadeoro1983@gmail.com>
2024-01-21 16:43:35 +0100

    create RoutesGuardian

--- src/pages/Escritorio/Escritorio.jsx
+++ src/pages/Escritorio/Escritorio.jsx
@@ -4,7 +4,6 @@ import Loading from '../../components/Loading/Loading'
 import { get } from '../../services/APIServices'
 
 import './Escritorio.css'
-import { useNavigate } from 'react-router-dom'
 
 const Escritorio = () => {
   const GridContainer = lazy(() => import('./partials/GridContainer'))
@@ -16,8 +15,6 @@ const Escritorio = () => {
     creargrupo: false
   })
 
-  const navigate = useNavigate()
-
   const [consumers, setConsumers] = useState([])
   const [consumerGroups, setConsumerGroups] = useState([])
   const [consumerGroup, setConsumerGroup] = useState(null)
@@ -25,31 +22,25 @@ const Escritorio = () => {
   const token = sessionStorage.getItem('token')
 
   useEffect(() => {
-    if (token) {
-      get('consumer')
-        .then((res) => setConsumers(res))
-        .catch((error) => console.log(error))
+    get('consumer')
+      .then((res) => setConsumers(res))
+      .catch((error) => console.log(error))
 
-      get('consumerGroup', token)
-        .then((res) => {
-          setConsumerGroups(res)
-          setConsumerGroup(res[0])
-        })
-        .catch((error) => console.log(error))
-    } else {
-      navigate('/')
-    }
+    get('consumerGroup', token)
+      .then((res) => {
+        setConsumerGroups(res)
+        setConsumerGroup(res[0])
+      })
+      .catch((error) => console.log(error))
   }, [])
 
-  return token ? (
+  return (
     <main className="mainContainer">
       <Nav {...{ escritorio, setEscritorio, consumerGroups, setConsumerGroup }} />
       <Suspense fallback={<Loading />}>
         <GridContainer {...{ consumerGroup, consumers, escritorio }} />
       </Suspense>
     </main>
-  ) : (
-    <div>WRONG.... no token</div>
   )
 }
 
