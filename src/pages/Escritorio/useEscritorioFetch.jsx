a1e5c51c2eda6219553898e18c0684487e1e093e
John Doe <fontanadeoro1983@gmail.com>
2024-01-24 21:58:34 +0100

    refactor

--- src/pages/Escritorio/useEscritorioFetch.jsx
+++ src/pages/Escritorio/useEscritorioFetch.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from 'react'
 import { get } from '../../services/APIServices'
 
-const useEscritorioFetch = () => {
+const useEscritorioFetch = (escritorio) => {
   const [consumers, setConsumers] = useState([])
   const [consumerGroups, setConsumerGroups] = useState([])
   const [consumerGroup, setConsumerGroup] = useState(null)
 
   useEffect(() => {
-    get('consumer')
-      .then((res) => setConsumers(res))
-      .catch((error) => console.log(error))
+    if (escritorio.consumidores)
+      get('consumer')
+        .then((res) => setConsumers(res))
+        .catch((error) => console.log(error))
 
-    get('consumerGroup')
-      .then((res) => {
-        setConsumerGroups(res)
-        setConsumerGroup(res[0])
-      })
-      .catch((error) => console.log(error))
-  }, [])
+    if (escritorio.gruposDeConsumo)
+      get('consumerGroup')
+        .then((res) => {
+          setConsumerGroups(res)
+          setConsumerGroup(res[0])
+        })
+        .catch((error) => console.log(error))
+  }, [escritorio])
 
   return { consumers, consumerGroup, setConsumerGroup, consumerGroups, setConsumerGroups }
 }
