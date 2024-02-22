0b90a8ab36a58d779d6272325da8d8d6fd1f946d
John Doe <fontanadeoro1983@gmail.com>
2024-02-22 21:02:50 +0100

    wip

--- src/pages/user/Escritorio/partials/HojasDeReparto/HojasDeReparto.jsx
+++ src/pages/user/Escritorio/partials/HojasDeReparto/HojasDeReparto.jsx
@@ -1,10 +1,17 @@
+import Grid from '../../../../../components/Grid/Grid'
+
 const HojasDeReparto = ({ castSheets }) => {
-  return (
-    <div>
-      <p>HojasDeReparto</p>
-      <p>{JSON.stringify(castSheets)}</p>
-    </div>
-  )
+  const columns = [{ field: 'consumerGroup' }, { field: 'date' }]
+  //console.log(castSheets)
+  const cellClickedListener = (event) => {
+    console.log('cellClicked', event)
+  }
+
+  const cellEditingStopped = ({ data, oldValue, newValue, column: { colId } }) => {
+    console.log(data)
+  }
+
+  return <Grid {...{ gridData: castSheets, columns, cellClickedListener, cellEditingStopped }} />
 }
 
 export default HojasDeReparto
--- src/pages/user/Escritorio/partials/RegistrosFinales/RegistrosFinales.jsx
+++ src/pages/user/Escritorio/partials/RegistrosFinales/RegistrosFinales.jsx
@@ -1,10 +1,24 @@
+import Grid from '../../../../../components/Grid/Grid'
+
 const RegistrosFinales = ({ finalRecords }) => {
-  return (
-    <div>
-      <p>RegistrosFinales</p>
-      <p>{JSON.stringify(finalRecords)}</p>
-    </div>
-  )
+  const columns = [
+    { field: 'supplementsKgs' },
+    { field: 'deliveredKgs', filter: true },
+    { filed: 'priceKgSuplements' },
+    { field: 'priceKg', filter: true },
+    { field: 'date' },
+    { field: 'totalEuros' }
+  ]
+
+  const cellClickedListener = (event) => {
+    console.log('cellClicked', event)
+  }
+
+  const cellEditingStopped = ({ data, oldValue, newValue, column: { colId } }) => {
+    console.log(data)
+  }
+
+  return <Grid {...{ gridData: finalRecords, columns, cellClickedListener, cellEditingStopped }} />
 }
 
 export default RegistrosFinales
