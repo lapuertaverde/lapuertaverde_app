826ef6c692f4d9c7586f9120c790e546f6363938
John Doe <fontanadeoro1983@gmail.com>
2024-02-28 21:44:44 +0100

    edit consumers

--- src/pages/user/Escritorio/partials/EditarConsumidor/EditConsumer.jsx
+++ src/pages/user/Escritorio/partials/EditarConsumidor/EditConsumer.jsx
@@ -13,17 +13,20 @@ import Avatar from '../../../../../components/Avatar/Avatar'
 import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
-import { patch } from '../../../../../services/APIServices'
+import { get, patch } from '../../../../../services/APIServices'
 import { toast } from 'react-toastify'
+import Button from '../../../../../components/Button/Button'
 
 const EditConsumer = ({ consumerGroups, setAlert }) => {
   const [allConsumers, setAllConsumers] = useState(consumersFlatter(consumerGroups))
   const [consumers, setConsumers] = useState(allConsumers)
+  const [favorites, setFavorites] = useState([])
 
   const grupos = consumerGroups.map((group) => group.name)
 
   const [open, setOpen] = useState(false)
   const [width, setWidth] = useState('100%')
+  const [drawerWidth, setDrawerWidth] = useState('280px')
 
   const defaultValues = {
     name: '',
@@ -53,7 +56,10 @@ const EditConsumer = ({ consumerGroups, setAlert }) => {
   }
 
   const onSubmit = (values) =>
-    patch(`consumer/${values._id}`, values)
+    patch(`consumer/${values._id}`, {
+      ...values,
+      favorites: ['65d9e544112ae95f4637a63b', '65d8f16e1a57081cb8d94b9c']
+    })
       .then(() => {
         toast.success('Consumidor Actualizado', { position: 'top-left' })
 
@@ -63,7 +69,11 @@ const EditConsumer = ({ consumerGroups, setAlert }) => {
         })
         setAllConsumers(consumersUpdated)
         setConsumers(
-          consumersUpdated.filter(({ groupName }) => watchGroups('groups') === groupName)
+          consumersUpdated.filter(({ groupName }) =>
+            watchGroups('groups') === 'Todos'
+              ? groupName !== watchGroups('groups')
+              : groupName === watchGroups('groups')
+          )
         )
       })
       .catch((error) =>
@@ -76,9 +86,25 @@ const EditConsumer = ({ consumerGroups, setAlert }) => {
       )
 
   useEffect(() => {
-    if (open) setWidth('calc(100% - 300px)')
+    if (open) setWidth('calc(100% - 290px)')
     else setWidth('100%')
   }, [open])
+
+  useEffect(() => {
+    if (drawerWidth === '890px' && watch('favorites').length)
+      watch('favorites').forEach((id) => {
+        get(`product/${id}`)
+          .then((res) => res && setFavorites([...favorites, res]))
+          .catch((error) =>
+            setAlert({
+              open: true,
+              type: 'error',
+              title: 'Error trayendo favoritos',
+              message: error.message
+            })
+          )
+      })
+  }, [drawerWidth])
 
   return (
     <div>
@@ -94,10 +120,9 @@ const EditConsumer = ({ consumerGroups, setAlert }) => {
               label="Filtrar por grupo"
               onChange={({ target: { value } }) => {
                 setConsumers(
-                  allConsumers.filter(({ groupName }) => {
-                    if (value === 'Todos') return groupName !== value
-                    else return groupName === value
-                  })
+                  allConsumers.filter(({ groupName }) =>
+                    value === 'Todos' ? groupName !== value : groupName === value
+                  )
                 )
               }}
             />
@@ -111,7 +136,7 @@ const EditConsumer = ({ consumerGroups, setAlert }) => {
       <Drawer
         {...{ open, onClose }}
         formId="editarConsumidorForm"
-        width="350px"
+        width={drawerWidth}
         drawerTitle={
           <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
             <Avatar src={watch('name') || ''} />
@@ -125,7 +150,7 @@ const EditConsumer = ({ consumerGroups, setAlert }) => {
               width: '100%',
               padding: '1rem',
               display: 'flex',
-              flexDirection: 'column',
+              flexWrap: 'wrap',
               gap: '1rem',
               justifyContent: 'center'
             }}
@@ -139,16 +164,49 @@ const EditConsumer = ({ consumerGroups, setAlert }) => {
             <InputSelect name="groupName" label="Grupo" options={grupos || []} />
             <InputNumber name="KgByDefault" label="Kg en cesta" />
             <Switcher name="active" label="Activo" />
-            <p>
-              orderInProgress :
-              {watch('orderInProgress') && JSON.stringify(watch('orderInProgress'))}
-            </p>
-            <p>orderFavs : {watch('orderFavs') && JSON.stringify(watch('orderFavs'))}</p>
-            <p>favorites : {watch('favorites') && JSON.stringify(watch('favorites'))}</p>
-            <p>discarded : {watch('discarded') && JSON.stringify(watch('discarded'))}</p>
-            <p>bills : {watch('bills') && JSON.stringify(watch('bills'))}</p>
-            <p>weeklyLog : {watch('weeklyLog') && JSON.stringify(watch('weeklyLog'))}</p>
           </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Button
+              type="button"
+              text={
+                drawerWidth === '890px'
+                  ? 'Ocultar Información Detallada'
+                  : 'Mostrar Información Detallada'
+              }
+              onClick={() => {
+                if (drawerWidth === '280px') {
+                  setWidth('300px')
+                  setDrawerWidth('1350px')
+                } else {
+                  setWidth('calc(100% - 290px)')
+                  setDrawerWidth('280px')
+                }
+              }}
+            />
+          </div>
+
+          {drawerWidth === '1350px' && (
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+              <p>
+                orderInProgress :
+                {watch('orderInProgress') && JSON.stringify(watch('orderInProgress'))}
+              </p>
+              FAVORITOS:
+              {favorites.length > 0 && (
+                <div style={{ border: '1px solid white', width: '100%', display: 'flex' }}>
+                  {favorites.map(({ name, priceKg, description, _id }) => (
+                    <div key={name}>
+                      <p>{_id}</p>
+                      <p>{name}</p>
+                    </div>
+                  ))}
+                </div>
+              )}
+              <p>discarded : {watch('discarded') && JSON.stringify(watch('discarded'))}</p>
+              <p>bills : {watch('bills') && JSON.stringify(watch('bills'))}</p>
+              <p>weeklyLog : {watch('weeklyLog') && JSON.stringify(watch('weeklyLog'))}</p>
+            </div>
+          )}
         </Form>
       </Drawer>
     </div>
