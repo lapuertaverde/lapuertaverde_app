7e739f0f1bb246cff0388ebaf745a5cb4366a1e4
John Doe <fontanadeoro1983@gmail.com>
2024-03-08 20:15:15 +0100

    edit consumidor WIP

---
+++ src/pages/user/Escritorio/partials/EditarConsumidor/editConsumerController.js
@@ -0,0 +1,11 @@
+export const defaultValues = {
+  name: '',
+  email: '',
+  dni: '',
+  address: '',
+  CP: '',
+  phone: '',
+  active: false,
+  groupName: '',
+  KgByDefault: 0
+}
--- src/pages/user/Escritorio/partials/EditarConsumidor/partials/MainEditConsumer/MainEditConsumer.jsx
+++ src/pages/user/Escritorio/partials/EditarConsumidor/partials/MainEditConsumer/MainEditConsumer.jsx
@@ -1,9 +1,16 @@
 import ConsumersCard from '../../../ConsumerCard/ConsumersCard'
 
-const MainEditConsumer = ({ consumers, onClick, watch, open, width }) => (
-  <div style={{ width }}>
-    <ConsumersCard {...{ consumers, onClick, id: !open ? null : watch('_id') }} />
-  </div>
-)
+const MainEditConsumer = ({ consumers, reset, watch, open, setOpen, width }) => {
+  const handleClick = (consumer) => {
+    reset(consumer)
+    if (!open) setOpen(true)
+  }
+
+  return (
+    <div style={{ width }}>
+      <ConsumersCard {...{ consumers, onClick: handleClick, id: !open ? null : watch('_id') }} />
+    </div>
+  )
+}
 
 export default MainEditConsumer
